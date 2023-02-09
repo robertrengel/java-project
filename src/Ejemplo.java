30b7fc889dec189e829f7384eb9c8cde52e50cd8
robertrengel <robertrengel@gmail.com>
2023-02-09 16:12:23 -0500

    project java 0.1

---
+++ src/Ejemplo.java
@@ -0,0 +1,8 @@
+public class Ejemplo {
+    public static void main(String[] args) {
+        int age = 20;
+        if (age >= 18) System.out.println("Es mayor de edad");
+        else System.out.println("Es menor de edad");
+    }
+    
+}
