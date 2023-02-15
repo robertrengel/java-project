54eb3ddcfe540f8657d64cb21c5da17c08eba0ca
robertrengel <robertrengel@gmail.com>
2023-02-15 17:50:50 -0500

    project java 0.7

--- src/CrearCuenta.java
+++ src/CrearCuenta.java
@@ -42,35 +42,7 @@ public class CrearCuenta {
         System.out.println("Java Classpath URL: " + System.getProperty("java.class.path.url"));
         System.out.println();
 
-        // Cliente clienteRobert = new Cliente();
-        //clienteRobert.setDocumento("123456789");
-        //cuentaRobert2.setTitular(clienteRobert);
-
-
-
-
-        // cuenta.numero = 0001;
-        // cuenta.titular = "Robert";
-        // cuenta.saldo = 100.0;
-        // cuenta.depositar(50.0);
-        
-        // Cuenta cuenta2 = new Cuenta();
-        // cuenta2.numero = 0002;
-        // cuenta2.titular = "jose";
-        // cuenta2.saldo = 200.0;
-        
-        // Cuenta cuenta3 = new Cuenta();
-        // cuenta3.numero = 0003;
-        // cuenta3.titular = "Ana";
-        // cuenta3.saldo = 300.0;
-        // System.out.println();
-        // System.out.println("Cuenta 1: " + cuenta.numero + " " + cuenta.titular + " " + cuenta.saldo);
-        // System.out.println("Cuenta 2: " + cuenta2.numero + " " + cuenta2.titular + " " + cuenta2.saldo);
-        // System.out.println("Cuenta 3: " + cuenta3.numero + " " + cuenta3.titular + " " + cuenta3.saldo);
-        // System.out.println();
-        // cuenta.retirar(100.0);
-        // System.out.println("Cuenta 1: " + cuenta.numero + " " + cuenta.titular + " " + cuenta.saldo);
-        // System.out.println();
+       
     }
 
 
